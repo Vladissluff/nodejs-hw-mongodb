109bde51cad45fab38bbdbe934e8da32b5e1467b
Vladissluff <165704824+Vladissluff@users.noreply.github.com>
2025-07-14 16:38:19 +0300

    hw-2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env.example' });
+dotenv.config({ path: '.env' });
 import { initMongoDB } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
