d0258dfe755b2b166e24bc46b3235dd7b5e4827a
Vladissluff <165704824+Vladissluff@users.noreply.github.com>
2025-07-15 22:15:50 +0300

    hw-2

--- src/server.js
+++ src/server.js
@@ -16,36 +16,35 @@ export const setupServer = () => {
     response.send('Welcome to "Contacts book". Please pass to /contacts');
   });
 
-  httpServer.get('/contacts', (request, response) => {
-    getAllContacts().then((result) =>
+ httpServer.get('/api/contacts', (request, response) => {
+  getAllContacts().then((result) =>
+    response.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: result,
+    }),
+  );
+});
+
+httpServer.get('/api/contacts/:contactId', (request, response) => {
+  const { contactId } = request.params;
+  getContactById(contactId)
+    .then((result) => {
+      if (result === null) throw new Error('404');
       response.status(200).json({
         status: 200,
-        message: 'Successfully found contacts!',
+        message: `Successfully found contact with id ${contactId}!`,
         data: result,
-      }),
-    );
-  });
-
-  httpServer.get('/contacts/:contactId', (request, response) => {
-    const { contactId } = request.params;
-    getContactById(contactId)
-      .then((result) => {
-        if (result === null) throw new Error('404');
-        response.status(200).json({
-          status: 200,
-          message: `Successfully found contact with id ${contactId}!`,
-          data: result,
-        });
-      })
-      .catch((error) => {
-        if (error.message === '404') {
-          response.status(404).json({ message: 'Contact not found' });
-        } else {
-          response.status(404).json({ message: 'Wrong ID' });
-        }
       });
-  });
-
+    })
+    .catch((error) => {
+      if (error.message === '404') {
+        response.status(404).json({ message: 'Contact not found' });
+      } else {
+        response.status(404).json({ message: 'Wrong ID' });
+      }
+    });
+});
   httpServer.use((request, response) => {
     response.status(404).json({
       message: 'Endpoint not found',
