a02cc71a28c6de7e2efe39ba50e5e7507b6462b0
Vladissluff <165704824+Vladissluff@users.noreply.github.com>
2025-07-25 21:46:57 +0300

    hw4

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,81 +7,66 @@ import {
   updateContactById,
 } from '../services/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
-export const getAllContactsController = ctrlWrapper(
-  async (request, response) => {
-    const result = await getAllContacts();
-    response.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: result,
-    });
-  },
-);
+export const getAllContactsController = ctrlWrapper(async (request, response) => {
+  const paginationParams = parsePaginationParams(request.query);
+  const sortParams = parseSortParams(request.query);
+  const filter = parseFilterParams(request.query);
 
-export const getContactByIdController = ctrlWrapper(
-  async (request, response) => {
-    const { contactId } = request.params;
-    if (contactId.length !== 24)
-      throw createHttpError(400, 'The ID must contain 24 characters.');
+  const result = await getAllContacts({ ...paginationParams, ...sortParams, ...filter });
+  if (result.data.length === 0) throw createHttpError(404, 'No matches found');
 
-    const result = await getContactById(contactId);
+  response.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: result,
+  });
+});
 
-    if (result === null) throw createHttpError(404, 'Contact not found');
+export const getContactByIdController = ctrlWrapper(async (request, response) => {
+  const { contactId } = request.params;
+  const result = await getContactById(contactId);
 
-    response.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: result,
-    });
-  },
-);
+  if (result === null) throw createHttpError(404, 'Contact not found');
 
-export const createContactController = ctrlWrapper(
-  async (request, response) => {
-    if (!!request.body.name === false || !!request.body.phoneNumber === false)
-      throw createHttpError(400, 'Name and phone number is required');
+  response.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: result,
+  });
+});
 
-    const newContact = await createContact(request.body);
+export const createContactController = ctrlWrapper(async (request, response) => {
+  const newContact = await createContact(request.body);
 
-    response.status(201).json({
-      status: 201,
-      message: 'Successfully created a contact!',
-      data: newContact,
-    });
-  },
-);
+  response.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: newContact,
+  });
+});
 
-export const updateContactByIdController = ctrlWrapper(
-  async (request, response) => {
-    const { contactId } = request.params;
-    if (contactId.length !== 24)
-      throw createHttpError(400, 'The ID must contain 24 characters.');
+export const updateContactByIdController = ctrlWrapper(async (request, response) => {
+  const { contactId } = request.params;
+  const result = await updateContactById(contactId, request.body);
 
-    if (!request.body) throw createHttpError(400, 'Missing data for update');
+  if (result === null) throw createHttpError(404, 'Contact not found');
 
-    const result = await updateContactById(contactId, request.body);
+  response.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result,
+  });
+});
 
-    if (result === null) throw createHttpError(404, 'Contact not found');
+export const deleteContactByIdController = ctrlWrapper(async (request, response) => {
+  const { contactId } = request.params;
+  const result = await deleteContactById(contactId);
 
-    response.status(200).json({
-      status: 200,
-      message: 'Successfully patched a contact!',
-      data: result,
-    });
-  },
-);
+  if (result === null) throw createHttpError(404, 'Contact not found');
 
-export const deleteContactByIdController = ctrlWrapper(
-  async (request, response) => {
-    const { contactId } = request.params;
-    if (contactId.length !== 24)
-      throw createHttpError(400, 'The ID must contain 24 characters.');
-
-    const result = await deleteContactById(contactId);
-
-    if (result === null) throw createHttpError(404, 'Contact not found');
-
-    response.status(204).send();
-  },
-);
+  response.status(204).send();
+});
