02b3b1eb9c780cf999affa80dfea5dac6ae5c222
Vladissluff <165704824+Vladissluff@users.noreply.github.com>
2025-08-09 17:53:13 +0300

    hw--6

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,7 +4,6 @@ import {
   logoutUserController,
   refreshUserController,
   registerUserController,
-<<<<<<< HEAD
   resetAuthPasswordController,
   sendResetEmailController,
 } from '../controllers/auth.js';
@@ -14,10 +13,6 @@ import {
   requestResetPwdBodyCheck,
   resetAuthPasswordBodyCheck,
 } from '../middlewares/validationBody.js';
-=======
-} from '../controllers/auth.js';
-import { loginUserBodyCheck, registerUserBodyCheck } from '../middlewares/validationBody.js';
->>>>>>> hw5-auth
 
 const authEndpoints = Router();
 
@@ -26,10 +21,7 @@ authEndpoints.post('/login', loginUserBodyCheck, loginUserController);
 authEndpoints.post('/refresh', refreshUserController);
 authEndpoints.post('/logout', logoutUserController);
 
-<<<<<<< HEAD
 authEndpoints.post('/send-reset-email', requestResetPwdBodyCheck, sendResetEmailController);
 authEndpoints.post('/reset-pwd', resetAuthPasswordBodyCheck, resetAuthPasswordController);
 
-=======
->>>>>>> hw5-auth
 export default authEndpoints;
